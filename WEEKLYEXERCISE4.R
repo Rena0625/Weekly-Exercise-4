101b9d82be0dfd44507a1c4077e82d9c74272006
Jiahe Ren <jren29@uic.edu>
2025-09-24 18:47:06 -0500

    Q6

---
+++ WEEKLYEXERCISE4.R
@@ -0,0 +1,19 @@
+
+library(readr)
+library(dplyr)
+
+olympics <- read_csv("Olympics.csv")
+
+olympics <- mutate(olympics, total.medals = gold + silver + bronze)
+
+medals_country <- group_by(olympics, country)
+medals_country <- summarise(medals_country,
+                            total_gold = sum(gold, na.rm = TRUE))
+
+medals_year <- group_by(olympics, year)
+medals_year <- summarise(medals_year,
+                         total_medals_given = sum(total.medals, na.rm = TRUE))
+
+head(olympics)
+head(medals_country)
+head(medals_year)
